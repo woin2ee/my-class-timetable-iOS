c6d7f6f78b2a9d39618ec34ee8ff4bfa1f117220
Jaewon <woin2ee@gmail.com>
2022-06-02 10:51:57 +0900

    Grouping

--- MyClassTimetable/ViewController/MainViewController.swift
+++ MyClassTimetable/ViewController/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  MyClassTimetable
 //
 //  Created by Jaewon on 2022/06/02.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
